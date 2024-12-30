792bff608f5d31f38910dcc8934cbb57f4e6b09b
PauLopez02 <1603995@uab.cat>
2024-12-30 21:20:07 +0100

    Tutorial (Maybe outadated screen_partition)

---
+++ frontend/milestone3/lib/screen_space.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'tree.dart';
+import 'request.dart' as rq;
+import 'package:milestone3/Data/data.dart';
+import 'user_info.dart';
+
+
+
+class ScreenSpace extends StatefulWidget {
+  final String id;
+  const ScreenSpace({super.key, required this.id});
+
+  @override
+  State<ScreenSpace> createState() => _ScreenSpaceState();
+}
+
+class _ScreenSpaceState extends State<ScreenSpace> {
+  late Future<Tree> futureTree;
+  @override
+  void initState() {
+    super.initState();
+    futureTree = rq.getTree(widget.id);
+  }
+
+  // future with listview
+  // https://medium.com/nonstopio/flutter-future-builder-with-list-view-builder-d7212314e8c9
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Tree>(
+      future: futureTree,
+      builder: (context, snapshot) {
+        // anonymous function
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              title: Text(snapshot.data!.root.id),
+              actions: <Widget>[
+                IconButton(icon: const Icon(Icons.home), onPressed: () {}
+                  // TODO go home page = root
+                ),
+                //TODO other actions
+              ],
+            ),
+            body: ListView.separated(
+              // it's like ListView.builder() but better because it includes a separator between items
+              padding: const EdgeInsets.all(16.0),
+              itemCount: snapshot.data!.root.children.length,
+              itemBuilder: (BuildContext context, int i) =>
+                  _buildRow(snapshot.data!.root.children[i], i),
+              separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // By default, show a progress indicator
+        return Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.white,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ));
+      },
+    );
+  }
+
+  Widget _buildRow(Door door, int index) {
+    return ListTile(
+      title: Text('D ${door.id}'),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min, // Ensures the Row takes up minimal space
+        children: [
+          Text('${door.state}, closed=${door.closed}'),
+          SizedBox(width: 8), // Optional spacing between text and button
+          IconButton(
+            icon: Icon(Icons.lock_open),
+            onPressed: ()  {
+              rq.openRequest("12345", "open", "2024-12-24T11:30", '${door.id}' );
+              setState(() {
+                this.futureTree = rq.getTree(widget.id);
+              });
+              // Action for the button
+              print('Unlock button pressed for Door ${door.id}');
+            },
+          ),
+          // Add more buttons if needed
+          IconButton(
+            icon: Icon(Icons.lock),
+            onPressed: () {
+              rq.openRequest("12345", "close", "2024-12-24T11:30", '${door.id}' ).then;
+              setState(() {
+                this.futureTree = rq.getTree(widget.id);
+              });
+              // Action for the button
+              print('Lock button pressed for Door ${door.id}');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+
+}
